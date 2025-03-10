3421919f9f603253ad34686b024059484de58f83
Mubarak <your-email@example.com>
2025-03-10 18:30:52 +0100

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+let profileMenu = document.getElementById("profileMenu");
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu")
+}
+
+let sidebarActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("ShowMoreLink");
+
+function toggleActivity(){
+    sidebarActivity.classList.toggle("open-activity");
+
+    if(sidebarActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "show less <b>-</b>";
+    }
+
+    else{
+        moreLink.innerHTML = "show more <b> +</b>";
+    }
+}
